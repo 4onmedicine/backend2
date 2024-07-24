349542e70bd0f68c669f3c14ea4a5f3b97ede8e5
jangwonjun <won2005won@naver.com>
2024-07-25 00:33:43 +0900

    [Fix] 처방전 인식 케이스 추가/정제과정 추가

--- modules/prescription.py
+++ modules/prescription.py
@@ -22,6 +22,24 @@ class Prescription:
         print("파일경로")
         print(self.filename)
         result = reader.readtext(self.filename)
+        
+        #print(result)
+
+        #약 품목 코드가 없는 경우 
+        include_keywords = ['정', '캡슐', '약', '정제']
+        exclude_keywords = ['복약안내', '약제비', '환자정보', '조제약사','노란색 정제','하얀색 정제','약국','의원','병원','흰색 정제','노랑색 정제','약품명','주의사항','약품사진','항갈색 정제']
+
+
+        filtered_data = []
+
+        # 약물 정보를 필터링하여 저장할 리스트
+        for bbox, text, confidence in result:
+        # 제외할 키워드가 포함되어 있지 않고, 포함할 키워드가 포함되어 있는 경우에만 추가
+            if not any(exclude_keyword in text for exclude_keyword in exclude_keywords) and \
+            any(include_keyword in text for include_keyword in include_keywords):
+                filtered_data.append((text))
+
+        print(filtered_data)
 
         # 약 품목 코드가 있는 경우 
         for detection in result:
